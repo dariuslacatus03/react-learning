869ee4510064d3b1ea360d7b4ef3c922e424636e
dariuslacatus03 <dariusandrey2003@gmail.com>
2024-03-04 00:23:10 +0200

    tic-tac-toe version 1

---
+++ tic-tac-toe/README.md
@@ -0,0 +1,29 @@
+### Tic-Tac-Toe Game in React
+
+Tic-Tac-Toe game using React that incorporates the following features:
+
+- **Playability:** Allows players to enjoy a game of Tic-Tac-Toe.
+  
+- **Victory Recognition:** Indicates when a player has won the game.
+
+- **History Tracking:** Stores a comprehensive history of the game as it progresses.
+
+- **Review Functionality:** Enables players to review the game's history, exploring previous versions of the game board.
+
+## TODO:
+
+✅ **Current Move Display:**
+   - Show "You are at move #..." instead of a button for the current move.
+
+✅ **Dynamic Board Creation:**
+   - Rewrite the `Board` component to use two loops for creating the squares instead of hardcoding them.
+
+✅ **Sorting Moves:**
+   - Add a toggle button that allows users to sort the moves in either ascending or descending order.
+
+✅ **Win Highlighting:**
+   - When someone wins, highlight the three squares that caused the win.
+   - Display a message about the result being a draw when no one wins.
+
+✅ **Move History Enhancement:**
+   - Display the location for each move in the format (row, col) in the move history list.
---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,149 @@
+// React provides a special function called useState that
+// you can call from your component to let it "remember"
+// things.
+import { useState } from "react";
+
+
+// This is a component. In React, a component is a piece
+// reusable code that represents a part of a user interface.
+// return - whatever comes after, is returned as a value
+//    to the caller of the function
+function Square({ value, onSquareClick }) {
+  return (
+    <button 
+      className="square"
+      onClick={onSquareClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+
+function Board({ xIsNext, squares, onPlay }) {
+  
+  function handleClick(i) {
+    //if the square was clicked before, return early
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    
+    //creates copy of squares array
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    onPlay(nextSquares)
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  return (
+    <>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
+}
+
+
+// export - makes the function accessible outside of the file
+// default - tells other files using the code that it's the
+//    main function in this file
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  //Array(n).fill(null) creates an array with n elements and sets them to null
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+
+  
+  function handlePlay(nextSquares) {
+    //spread operator (...): enumerates all the items in history
+    //.slice(0, n): gets the elements from index 0 to index n
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+  }
+  //as we iterate through history array, the squares argument
+  //goes through each element of history, and the move argument
+  //goes through each array index: 0, 1, 2, ...
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      //it’s strongly recommended that you assign proper keys 
+      //whenever you build dynamic lists
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+
+// It does not matter wether we devine calculateWinner before or
+// after the Board.
+function calculateWinner(squares) {
+  const winningLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < winningLines.length; i++) {
+    const [a, b, c] = winningLines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
